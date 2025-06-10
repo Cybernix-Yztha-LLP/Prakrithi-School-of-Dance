9e8ac5e6df2b888e9200183c31ec609a7f7f415f
yashasrnair <yashasrnair@gmail.com>
2025-06-10 23:15:31 +0530

    Test deployment7

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,7 +39,7 @@ const Navbar = () => {
             className="font-labrada font-medium text-[#636161] text-xl sm:text-2xl md:text-3xl lg:text-[40px] text-center md:text-left no-underline"
             onClick={handleHomeClick}
           >
-            kuruvi pro max ultimate king god version 
+            kuruvi pro max ultimate king god version test7
           </Link>
 
           {/* Hamburger Menu Button - visible on small screens */}
